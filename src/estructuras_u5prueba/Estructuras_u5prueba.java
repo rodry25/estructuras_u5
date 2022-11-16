ddc0f9b2db0cb23f79049da1184b7def7e08cd95
yahir <yahir@GWNR71517>
2022-11-16 14:44:15 -0700

    carga inicial carga final

--- src/estructuras_u5prueba/Estructuras_u5prueba.java
+++ src/estructuras_u5prueba/Estructuras_u5prueba.java
@@ -16,6 +16,9 @@ public class Estructuras_u5prueba {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola Mundo");
+        
+        
     }
     
 }
